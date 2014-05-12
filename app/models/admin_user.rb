9958cfbf8059b7e637320b0e27ad15e1d0fcd2e6
Brian O'Sullivan <brian@tykus.ie>
2014-05-12 12:08:46 +0800

    Creating has_many through associations to traverse rich join

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,4 +2,6 @@ class AdminUser < ActiveRecord::Base
 
   has_and_belongs_to_many :pages, :join_table => "admin_users_pages"
   has_many :section_edits
+  has_many :sections, :through => :section_edits
+
 end
--- app/models/section_edits.rb
+++
@@ -1,2 +0,0 @@
-class SectionEdits < ActiveRecord::Base
-end
