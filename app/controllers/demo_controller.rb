79fc8a3c4c62288c88de9142586c4e4c96328991
Brian O'Sullivan <brian@tykus.ie>
2014-05-15 10:33:36 +0800

    Improving descriptions in the demo pages

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -6,8 +6,24 @@ class DemoController < ApplicationController
   end
 
   def text_helpers
+    @text = """
+            This is some long sample text. 
+            It includes line breaks\nand double-line breaks.\n\n
+            It rambles on and on and on.\n\n Until we get to this last line.
+          """
+
   end
 
   def number_helpers
+  end
+
+  def sanitize
+    @text = """
+              <p>
+                <strong>This is a string</strong> with various HTML <em>tags</em> and 
+                <a href=\"#\">links</a> included inside
+              </p>
+              <script>alert(\"Hello there\")</script>
+            """
   end
 end
