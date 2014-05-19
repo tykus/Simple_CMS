c25554ab82057e766bee0aba5c78852c379bd349
Brian O'Sullivan <brian@tykus.ie>
2014-05-19 14:47:29 +0800

    Adding basic nesting be passing subject_id in every Pages Controller request

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,10 @@ class PagesController < ApplicationController
   layout "admin"
 
   before_action :confirm_logged_in
+  before_action :find_subject
 
   def index
-    @pages = Page.sorted
+    @pages = @subject.pages.sorted
   end
 
   def show
@@ -22,7 +23,7 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
     if @page.update_attributes(page_params)
       flash[:notice] = "Page updated successfully"
-      redirect_to(:action => 'show', :id => @page.id)
+      redirect_to(:action => 'show', :id => @page.id, :subject_id => @subject.id)
     else
       @page_count = Page.count
       @subjects = Subject.sorted
@@ -31,7 +32,7 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new
+    @page = Page.new({:subject_id => @subject.id, :name => "Default"})
     @subjects = Subject.sorted
     @page_count = Page.count + 1    
   end
@@ -40,7 +41,7 @@ class PagesController < ApplicationController
     @page = Page.new(page_params)
     if @page.save
       flash[:notice] = "Page created successfully"
-      redirect_to(:action => 'index')
+      redirect_to(:action => 'index', :subject_id => @subject.id)
     else
       @subjects = Subject.sorted
       @page_count = Page.count + 1
@@ -55,12 +56,18 @@ class PagesController < ApplicationController
   def destroy
     page = Page.find(params[:id]).destroy
     flash[:notice] = "Page '#{page.name}' deleted successfully."
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :subject_id => @subject.id)
   end
 
   private
     def page_params
       params.require(:page).permit(:name,:permalink,:position,:visible,:subject_id)
+    end
+
+    def find_subject
+      if params[:subject_id]
+        @subject = Subject.find(params[:subject_id])
+      end
     end
 
 end
