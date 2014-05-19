cc36bb1e7d5387b769d24967fdb5db459171a00c
Brian O'Sullivan <brian@tykus.ie>
2014-05-19 09:13:12 +0800

    Specifying controller in login redirect after move to applications_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   def confirm_logged_in
     unless session[:user_id]
       flash[:notice] = "Please log in."
-      redirect_to(:action => 'login')
+      redirect_to(:controller => 'access', :action => 'login')
       return false #halts the before_action
     else
       return true
